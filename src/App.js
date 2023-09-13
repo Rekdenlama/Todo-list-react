3b82e84375556417633f7b9cf26df5f9389dbb54
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2023-09-13 18:18:42 +0530

    bug fixed in add item

--- src/App.js
+++ src/App.js
@@ -1,80 +1,78 @@
-
-import Header from './Header';
+import { useState } from 'react';
 import AddItem from './AddItem';
-import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
-import {useState} from 'react';
-
+import Header from './Header';
+import SearchItem from './SearchItem';
 
 function App() {
-const [items, setItems]=useState(JSON.parse(localStorage.getItem('shoppingList')));
-const[search,setSearch]=useState('');
-const [newItem, setNewItem]= useState(''); 
-const setAnsSaveItems= (newItems)=>{
-  setItems(newItems);
-  localStorage.setItem('shoppingList', JSON.stringify(newItems));
-}
+  const shoppingList = JSON.parse(localStorage.getItem('shoppingList'));
+  const [items, setItems] = useState(shoppingList ? shoppingList : []);
+  const [search, setSearch] = useState('');
+  const [newItem, setNewItem] = useState('');
+  const setAnsSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppingList', JSON.stringify(newItems));
+  };
 
-const addItem=(item)=>{
-  console.log(item)
-  const id=items.length ? items[items.length-1].id + 1 : 1;
-  const myNewItem={id,checked:false, item};
-  const listItems= [...items, myNewItem];
-  setAnsSaveItems(listItems);
-}
+  const addItem = (item) => {
+    const id = items?.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    if (items) {
+      const listItems = [...items, myNewItem];
+      setAnsSaveItems(listItems);
+    } else {
+      setAnsSaveItems([myNewItem]);
+    }
+  };
 
-const handleCheck=(id)=>{
-  if(!items) return;
-  const listItems=items.map((item)=> item.id===id ?{ ...item,checked: !item.checked}: item)
-  setAnsSaveItems(listItems);
-}
+  const handleCheck = (id) => {
+    if (!items) return;
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setAnsSaveItems(listItems);
+  };
 
-// function handleDelete(id){
-//   const listItems=items.filter(item=>{
-//       return item.id !==id;
-//   });
-//       setItems(listItems);
-//       localStorage.setItem('shoppingList', JSON.stringify(listItems));
-  
-// }
+  // function handleDelete(id){
+  //   const listItems=items.filter(item=>{
+  //       return item.id !==id;
+  //   });
+  //       setItems(listItems);
+  //       localStorage.setItem('shoppingList', JSON.stringify(listItems));
 
-const handleDelete=(id)=>{
-  if(!items) return;
-  const listItems=items.filter((item)=>item.id !==id)
-  setAnsSaveItems(listItems);
-}
+  // }
 
-//we use e so that it doesn't refresh
-const handleSubmit=(e)=>{
-  e.preventDefault();
-  if(!newItem) return;
-  addItem(newItem);
-  setNewItem('');
-}
+  const handleDelete = (id) => {
+    if (!items) return;
+    const listItems = items.filter((item) => item.id !== id);
+    setAnsSaveItems(listItems);
+  };
+
+  //we use e so that it doesn't refresh
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  };
   return (
     <div className="App">
-      <Header title="Grocery List"
-      />
-      <AddItem 
+      <Header title="Grocery List" />
+      <AddItem
         newItem={newItem}
         setNewItem={(value) => {
           setNewItem(value);
         }}
-        handleSubmit={handleSubmit}   
+        handleSubmit={handleSubmit}
       />
-      <SearchItem 
-        search={search}
-        setSearch={setSearch}
-      />
-      <Content 
+      <SearchItem search={search} setSearch={setSearch} />
+      <Content
         items={items}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
-      {/* <Footer title="Copyright ©"
-      length={items.length}
-      /> */}
+      <Footer title="Copyright ©" length={items.length} />
     </div>
   );
 }
